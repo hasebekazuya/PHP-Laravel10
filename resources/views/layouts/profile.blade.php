708f6ce8b2e0824ca7059713799fa2132be9405c
hasebekazuya <tabi85nack5877@icloud.com>
2022-12-21 12:33:05 +0000

    PHP/Laravel11/課題完了

---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<htmi lang="{{ app()->getLocal() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        
+        <meta name="csrf-token" content="{{ csrf token() }}">
+        
+        <title>@yield('title')</title>
+        
+        <script src="{{ seure asset('js/app.js') }}" defer></script>
+        
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link rel="https:fonts.gooleapis.com/css?family=Raleway:300,400,600"
+rel="stylesheet" type="text/css">
+
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        
+                    </a>
+                    <button class="navbar-toggler" type="button" date-toggler="collapse" date-
+target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+aria-label="Toggel navigation">
+                        <span clss="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nar ms-auto">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <mian class="py-4">
+                @yield('content')
+            </mian>
+        </div>
+    </body>
+</htmi>
