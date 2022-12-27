ca4cbf33a3b9d0a3a76b4023f666a38e545b6551
hasebekazuya <tabi85nack5877@icloud.com>
2022-12-27 12:45:32 +0000

    PHP/Laravel.13課題完了

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,47 @@
         <div class="row">
             <div class="col-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipar/form-data">
+                    
+                    @if (count($errors) > 0 )
+                         <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                     @endif
+                     <div class="form-group row">
+                         <label class="col-md-2">氏名</label>
+                         <div class="clo-md-10">
+                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                         </div>
+                     </div>
+                     <div class="form-group row">
+                         <label>性別</label>
+                         <br>
+                        <div class="col-md-10">
+                            <input type="radio" name="gender" value="男性">男性
+                        </div>
+                        
+                            <div class="coi-md-10">
+                            <input type="radio" name="gender" value="女性">女性
+                            </div>
+                            
+                        <div class="form-group row">
+                            <label class="col-md-2">趣味</label>
+                            <div class="col-md-10">
+                                <textarea class="form-control" name="hobby" row="10" cols="10">{{ old('hobby') }}</textarea>
+                            </div>
+                        </div>
+                        <div class='form-group row'>
+                            <label class="col-md-2">自己紹介</label>
+                            <div class="col-md-10">
+                                <textarea class="form-control" name="introduction" row="20" cols="20">{{ old('introduction') }}</textarea>
+                            </div>
+                        </div>
+                     </div>
+                     @csrf
+                </form>
             </div>
         </div>
     </div>
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<htmi lang="{{ app()->getLocal() }}">
+<htmi lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
         
-        <meta name="csrf-token" content="{{ csrf token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         
         <title>@yield('title')</title>
         
-        <script src="{{ seure asset('js/app.js') }}" defer></script>
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
         
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link rel="https:fonts.gooleapis.com/css?family=Raleway:300,400,600"
